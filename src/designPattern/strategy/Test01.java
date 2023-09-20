11cac487f236d35c1f18a2c6e1dbf37f445be76b
김용현 <yh.kim73@cj.net>
2023-09-20 10:21:13 +0900

    BBB

--- src/designPattern/strategy/Test01.java
+++ src/designPattern/strategy/Test01.java
@@ -3,6 +3,9 @@ package designPattern.strategy;
 public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("sdfdsf");
+		System.out.println("VVV");
+		
+		System.out.println("CCC");
 	}
 
 }
